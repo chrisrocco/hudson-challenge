62b685f63681e794ff225ca63dd882e433b856c3
cfalcione <caleb.falcione@gmail.com>
2017-11-04 15:05:55 -0500

    Removed Junk

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Beanalli Coffee'),
+    'name' => env('APP_NAME', 'Laravel'),
 
     /*
     |--------------------------------------------------------------------------
