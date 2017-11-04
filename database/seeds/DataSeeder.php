d4f69d312e3a50836e70a9bd17cd253586d76d1f
cfalcione <caleb.falcione@gmail.com>
2017-11-04 15:36:23 -0500

    Data progress

--- database/seeds/DataSeeder.php
+++ database/seeds/DataSeeder.php
@@ -5,6 +5,13 @@ use Illuminate\Database\Seeder;
 class DataSeeder extends Seeder {
 
     public function run() {
-        $file_contents = file_get_contents("");
+        $file = __DIR__ . "/../../resources/input_data.csv";
+        $csv = array_map('str_getcsv', file($file));
+        array_walk($csv, function(&$a) use ($csv) {
+            $a = array_combine($csv[0], $a);
+        });
+        array_shift($csv); # remove column header
+
+
     }
 }
