5c8a5db3ea5e48750984139f01274291579e59a1
cfalcione <caleb.falcione@gmail.com>
2017-11-05 09:09:15 -0600

    added odds scale

--- app/StudyGene.php
+++ app/StudyGene.php
@@ -13,9 +13,19 @@ class StudyGene extends Model {
         'allele',
     ];
 
+    protected $appends = [
+        'odds_scale'
+    ];
+
     protected $table = "studies_genes";
 
     public function study() {
         return $this->belongsTo(Study::class, 'study_id');
+    }
+
+    public function getOddsScaleAttribute() {
+        $ratio = $this->odds_ratio;
+        $ratio = max(0.1, min(10, $ratio)); //clamp between 0.1 and 10
+        return log10($ratio);
     }
 }
